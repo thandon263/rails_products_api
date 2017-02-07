467f2063ccb0442d1792a81f70d8f033a93a0a7b
thandon263 <ncubethando20@gmail.com>
2017-02-07 15:11:40 -0500

    Created authentication of user tokens.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
     has_many :music
+
+    # Assign an API Key on create
+    before_create do |user|
+        user.api_key = user.generate_api_key
+    end
+
+    # Generating the api_key
+    def generate_api_key
+        loop do
+            token = SecureRandom.base64.tr('+/=', 'Qrt')
+            break token unless User.exists?(api_key: token)
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
   
-  constraints subdomain: 'api' do
-    scope module: 'api' do
-      namespace :v1 do
-          resources :users do
-            resources :musics
-         end
-       end
-    end
-  end
-
+  resources :users
+  resources :musics
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
